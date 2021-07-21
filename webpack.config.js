c2880ece7ab77c928d5939b1e38e36f3f4e24820
Kevin Minutti <minutti70@gmail.com>
2021-07-21 13:46:31 -0400

    Debug babel/webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,20 @@
 "use strict";
-const { resolve } = require("path");
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: ["./src/app.js"],
   output: {
     path: __dirname,
     filename: "./public/bundle.js",
   },
   mode: "development",
   context: __dirname,
-  //devtool: "source-map"
   resolve: {
     extensions: [".js", ".jsx"],
   },
   module: {
     rules: [
       {
-        test: /jsx?$/,
-        include: resolve(__dirname, "./src"),
+        test: /\.(js|jsx)$/,
         loader: "babel-loader",
       },
       {
