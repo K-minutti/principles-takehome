6c9f5774b1b73682d73e46476ab261730b2322f8
Kevin Minutti <minutti70@gmail.com>
2021-07-21 14:10:04 -0400

    Refactor express app to listen in separate file

---
+++ server/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// API Route - Dog API
+app.use("/api", require("./api"));
+
+// Serving static files
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Requests with extensions (.js .ccs etc.) sending 404
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  } else {
+    next();
+  }
+});
+
+// Wildcard-base sends index.html
+app.get("*", (req, res) => {
+  res.sendFile(__dirname, "../public/index.html");
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send(err.message || "Internal Server Error");
+});
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,43 +1,7 @@
-const express = require("express");
-const path = require("path");
-
-const app = express();
-
-// Body parsing middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// API Route - Dog API
-app.use("/api", require("./api"));
-
-// Serving static files
-app.use(express.static(path.join(__dirname, "../public")));
-
-// Requests with extensions (.js .ccs etc.) sending 404
-app.use((req, res, next) => {
-  if (path.extname(req.path).length) {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
-  } else {
-    next();
-  }
-});
-
-// Wildcard-base sends index.html
-app.get("*", (req, res) => {
-  res.sendFile(__dirname, "../public/index.html");
-});
-
-// Error handling
-app.use((err, req, res, next) => {
-  res.status(err.status || 500).send(err.message || "Internal Server Error");
-});
-
-const PORT = process.env.PORT || 3000;
+const app = require("./app");
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}.
-  http://localhost:${PORT}/
-  `);
+  http://localhost:${PORT}`);
 });
